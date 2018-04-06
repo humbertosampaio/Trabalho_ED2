cd2a08ff634e6573d65308d96451dd3a73031c21
viniman <vinicius.carlos@live.com>
2018-04-06 16:20:32 -0300

    Testando impressão do algoritmo

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include "Headers/FileUtils.h"
-#include <ctime>
 
 using namespace std;
 int main(int argc, char** argv)
 {
-    string path = "/home/edson/pythonquestions/Questions.csv";
+    //string path = "/home/edson/pythonquestions/Questions.csv"; //edson
+    string path = "/media/viniman/Files/Google Drive/UFJF/2018/1/ED2/Trabalho/pythonquestions/Questions.csv";
     vector<Question*>* list = FileUtils::readFileQuestion(path);
+
+    for(unsigned int i = 0; i < 10; i++)
+        list->at(i)->printComponents();
+
     return 0;
 }
