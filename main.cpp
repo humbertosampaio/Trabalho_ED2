9f048bcc1c1403b54ab2d5c238dba072132beb57
Humberto Sampaio <humberto.osampaio@gmail.com>
2018-04-15 22:37:31 -0300

    Começo da implementação do menu e adição arquivos de entrada e saida

--- main.cpp
+++ main.cpp
@@ -16,19 +16,24 @@ void sortQuestions(vector<Question> &questionVector);
 void sortIntegers(vector<int> &intVector);
 vector<int> getVetQuestionIdAleatorios(const vector<Question> &listQuestions, const int &n);
 template<class T> void printVector(const vector<T> &vector);
+template<class T> void openMenu(vector<T> &vector);
 
 int main(int argc, char** argv)
 {
-	//string path = "/home/edson/pythonquestions/Questions.csv"; //edson
-	//string path = "/media/viniman/Files/Google Drive/UFJF/2018/1/ED2/Trabalho/pythonquestions/Questions.csv";
-	string path = "../pythonquestions/OriginalFiles/Questions.csv";
-
+	string sourceFileName = argv[1];
 	vector<Question> listQuestions;
+
+	string path = "../pythonquestions/OriginalFiles/Questions.csv";
 	FileUtils::readFileQuestion(path, listQuestions);
 	sortQuestions(listQuestions);
+//	openMenu(listQuestions);
 
-	vector<int> vetQuestionId = getVetQuestionIdAleatorios(listQuestions, 20);
-	sortIntegers(vetQuestionId);
+	//string path = "/home/edson/pythonquestions/Questions.csv"; //edson
+	//string path = "/media/viniman/Files/Google Drive/UFJF/2018/1/ED2/Trabalho/pythonquestions/Questions.csv";
+
+//
+//	vector<int> vetQuestionId = getVetQuestionIdAleatorios(listQuestions, 20);
+//	sortIntegers(vetQuestionId);
 
 	return 0;
 }
@@ -52,28 +57,25 @@ void sortIntegers(vector<int> &intVector)
 	random_shuffle(intVector.begin(), intVector.end());
 
 	cout << "Vetor Original:" << endl;
-	printVector (intVector);
+	printVector(intVector);
 	HeapSort::heapSort(intVector);
 
 	cout << "Vetor Ordenado:" << endl;
 	printVector(intVector);
 }
 
-vector<int> getVetQuestionIdAleatorios(const vector<Question> &listQuestions,
-		const int &n)
+vector<int> getVetQuestionIdAleatorios(const vector<Question> &listQuestions, const int &n)
 {
 	vector<int> vetQuestionId;    // = new vector<int>;
 	long seed = std::chrono::system_clock::now().time_since_epoch().count();
 	mt19937 eng(seed);    ///mersenne twister engine
-	uniform_int_distribution<unsigned long> distAleatoria(0,
-			listQuestions.size() - 1);
+	uniform_int_distribution<unsigned long> distAleatoria(0, listQuestions.size() - 1);
 
 	//srand(static_cast<unsigned int>(time(nullptr)));
 
 	for (int i = 0; i < n; i++)
 	{
-		vetQuestionId.emplace_back(
-				listQuestions[distAleatoria(eng)].getQuestionId());
+		vetQuestionId.emplace_back(listQuestions[distAleatoria(eng)].getQuestionId());
 		//vetQuestionId[i] = listQuestions->at(rand()%(listQuestions->size()-1))->getQuestionId();
 	}
 
@@ -86,3 +88,42 @@ template<class T> void printVector(const vector<T> &vector)
 		cout << it << " ";
 	cout << endl << endl;
 }
+
+void printSections()
+{
+	cout << "Escolha a secao: " << endl;
+	cout << "Secao 1: Analise dos Algoritmos" << endl;
+	cout << "Secao 2: Implementacao das Tags Frequentes e dos Usuarios Ativos" << endl;
+}
+
+void printCenaries_Section1()
+{
+	cout << "\tCenario 1: Elementos do tipo Inteiro" << endl;
+	cout << "\tCenario 2: Elementos do tipo Question" << endl;
+}
+
+template<class T> void openMenu(vector<T> &vector)
+{
+	int secao, cenario, N = 10; // N será lido do arquivo "entrada.txt"
+	string path = "../pythonquestions/OriginalFiles/Questions.csv";
+	printSections();
+	cin >> secao;
+	switch (secao)
+	{
+		case 1:
+			printCenaries_Section1();
+			cin >> cenario;
+			switch (cenario)
+			{
+				case 1:
+					getVetQuestionIdAleatorios(vector, N);
+					// Chamar QuickSort para inteiros
+					break;
+				case 2:
+					vector = FileUtils::readFileQuestion(path, vector);
+			}
+
+	}
+}
+
+
